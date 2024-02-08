1131747df8d345f029e27d94b23c866c5c3a7238
AVasilaki <poseydon463@gmail.com>
2024-02-08 18:55:01 +0200

    i morgan

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
 const contactsRouter = require("./routes/contactsRouter");
 const authRouter = require("./routes/authRouter");
 require("dotenv").config();
@@ -6,6 +8,9 @@ require("dotenv").config();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use(morgan("tiny"));
+app.use(express.static("public"));
 
 app.use("/api/contacts", contactsRouter);
 app.use("/api/auth", authRouter);
